22e32f28f4a624ef0258c2d9675fa0cca0c34db7
ciccioxm3 <cicciariello@proton.me>
2025-05-15 14:23:08 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -9,9 +9,10 @@ import time
 
 LINKMFP = "https://tuonome.hf.space"
 PASSWORD = "tuapassword"
-
+REFERER = "forcedtoplay.xyz"
+ORIGIN = "forcedtoplay.xyz"
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
-HEADER = "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Fcaq21harderv991gpluralplay.xyz%2F&h_origin=https%3A%2F%2Fcaq21harderv991gpluralplay.xyz"
+HEADER = f"&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2F{REFERER}%2F&h_origin=https%3A%2F%2F{ORIGIN}"
 M3U8_OUTPUT_FILE = "247ita.m3u8"
 # File e URL statici
 daddyLiveChannelsFileName = '247channels.html'
