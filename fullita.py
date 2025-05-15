e6449577a1c4a71e2b52b4b54e11b96c0be54787
ciccioxm3 <cicciariello@proton.me>
2025-05-15 14:23:39 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -14,10 +14,11 @@ import time
 
 LINKMFP = "https://tuonome.hf.space"
 PASSWORD = "tuapassword"
-
+REFERER = "forcedtoplay.xyz"
+ORIGIN = "forcedtoplay.xyz"
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
-HEADER = "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Fcaq21harderv991gpluralplay.xyz%2F&h_origin=https%3A%2F%2Fcaq21harderv991gpluralplay.xyz"
+HEADER = f"&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2F{REFERER}%2F&h_origin=https%3A%2F%2F{ORIGIN}"
 
 NUM_CHANNELS = 1000
 DADDY_JSON_FILE = "daddyliveSchedule.json"
