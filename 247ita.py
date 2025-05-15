84f98ceb31873f4716d26aa0124ad464003e4da5
ciccioxm3 <cicciariello@proton.me>
2025-05-15 11:26:00 +0200

    Update 247ita.py

--- 247ita.py
+++ 247ita.py
@@ -11,7 +11,7 @@ LINKMFP = "https://tuonome.hf.space"
 PASSWORD = "tuapassword"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
-HEADER = "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Fxextreamplay.xyz%2F&h_origin=https%3A%2F%2Fxextreamplay.xyz"
+HEADER = "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Fcaq21harderv991gpluralplay.xyz%2F&h_origin=https%3A%2F%2Fcaq21harderv991gpluralplay.xyz"
 M3U8_OUTPUT_FILE = "247ita.m3u8"
 # File e URL statici
 daddyLiveChannelsFileName = '247channels.html'
