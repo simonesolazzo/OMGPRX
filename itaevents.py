a81d09d09c09b2f186a9b147b36af6a25176c2cd
ciccioxm3 <cicciariello@proton.me>
2025-05-15 11:26:34 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -18,7 +18,7 @@ PASSWORD = "tuapassword"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
-HEADER = "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Fxextreamplay.xyz%2F&h_origin=https%3A%2F%2Fxextreamplay.xyz"
+HEADER = "&h_user-agent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F133.0.0.0+Safari%2F537.36&h_referer=https%3A%2F%2Fcaq21harderv991gpluralplay.xyz%2F&h_origin=https%3A%2F%2Fcaq21harderv991gpluralplay.xyz"
 
 NUM_CHANNELS = 10000
 DADDY_JSON_FILE = "daddyliveSchedule.json"
