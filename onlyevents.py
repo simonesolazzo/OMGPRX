793b442807ea4f74f750339ccd00dd965b0f3131
simonesolazzo <35707697+simonesolazzo@users.noreply.github.com>
2025-05-18 17:02:32 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -10,8 +10,8 @@ from bs4 import BeautifulSoup
 import time
 # Costanti
 
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://gscacchi-michi.hf.space"
+PASSWORD = "Simone1234."
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 REFERER = "forcedtoplay.xyz"
